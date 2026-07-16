ec224fcd1f8c60de4209072820d83c8e24f02f34
plucafs <plucafz@proton.me>
2026-07-16 16:47:32 +0200

    Changed texts, texts colors and layout

--- project-example/main.gd
+++ project-example/main.gd
@@ -1,6 +1,12 @@
 extends Control
 
 @onready var keys: Label = %Keys
+@onready var line_edit_autocomplete: AutocompleteLineEdit = %LineEditAutocomplete
+@onready var words: Label = %Words
+
+func _ready() -> void:
+	line_edit_autocomplete.grab_focus()
+	words.text = "Try: " + " | ".join(line_edit_autocomplete.words_list)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
