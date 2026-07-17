cb054b1b61d3975c85a79580aa6547b1155fb816
plucafs <plucafz@proton.me>
2026-07-17 02:59:28 +0200

    Added checkbox for debug purposes

--- project-example/main.gd
+++ project-example/main.gd
@@ -3,8 +3,15 @@ extends Control
 @onready var keys: Label = %Keys
 @onready var line_edit_autocomplete: AutocompleteLineEdit = %LineEditAutocomplete
 @onready var words: Label = %Words
+@onready var case_insensitive_check_box: CheckBox = %CaseInsensitiveCheckBox
 
 func _ready() -> void:
+	case_insensitive_check_box.toggled.connect(
+		func(state: bool):
+			line_edit_autocomplete.case_insensitive = state
+	)
+
+	case_insensitive_check_box.button_pressed = line_edit_autocomplete.case_insensitive
 	line_edit_autocomplete.grab_focus()
 	words.text = "Try: " + " | ".join(line_edit_autocomplete.words_list)
 
